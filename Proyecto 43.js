51fcd449843dbfde5a2fa3b5d16c02235ac8679d
Urrruck <90486098+Urrruck@users.noreply.github.com>
2022-07-07 21:25:39 -0500

    Add files via upload

---
+++ Proyecto 43.js
@@ -0,0 +1,11 @@
+const accessCode1 = "VARIABLE";
+const accessCode2 = "FUNCTION";
+const accessCode3 = "DATABASE";
+// add code for displaying rest of the names and the hints.
+function clues() {
+    fill("white")
+    textSize(15)
+    text("R E V B A I L A", 100,50)
+    fill("lightblue")
+    text("Hint: Always changing, not constant !!", 100,70)
+}
